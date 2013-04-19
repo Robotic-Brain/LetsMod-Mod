4ee50935e03d273dc8af07a5730d521ad2278dae
Robotic-Brain <github@roboticbrain.de>
2013-04-19 11:21:50 +0200

    Updates from Episode 5

---
+++ letsmod_common/de/roboticbrain/letsmod/lib/Reference.java
@@ -0,0 +1,17 @@
+package de.roboticbrain.letsmod.lib;
+
+/**
+ * Lets Mod
+ * 
+ * Reference
+ * 
+ * @author Robotic-Brain
+ * @author pahimar
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+public class Reference {
+    public static final String MOD_ID = "LetsMod";
+    public static final String MOD_NAME = "Let's Mod";
+    public static final String VERSION_STRING = "0.0.1";
+}
